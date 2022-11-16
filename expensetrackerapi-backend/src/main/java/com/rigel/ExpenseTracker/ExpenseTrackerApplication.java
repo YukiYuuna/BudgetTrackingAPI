5798409244afabe17033ffbfe25dc3a4fb81f416
KonstantinMB <konstantinmb@yahoo.com>
2022-11-16 22:02:27 +0200

    Revert newer implementation to functional program.

--- expensetrackerapi-backend/src/main/java/com/rigel/ExpenseTracker/ExpenseTrackerApplication.java
+++ expensetrackerapi-backend/src/main/java/com/rigel/ExpenseTracker/ExpenseTrackerApplication.java
@@ -3,21 +3,14 @@ package com.rigel.ExpenseTracker;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.messaging.Message;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.function.Consumer;
-
 @Slf4j
 @SpringBootApplication
-@EnableEurekaClient
 public class ExpenseTrackerApplication {
 
 	public static void main(String[] args){
@@ -25,18 +18,8 @@ public class ExpenseTrackerApplication {
 	}
 
 	@Bean
-	public RestTemplate restTemplate(RestTemplateBuilder builder) {
-		return builder.build();
-	}
-
-	@Bean
 	PasswordEncoder passwordEncoder(){
 		return new BCryptPasswordEncoder();
-	}
-
-	@Bean
-	public Consumer<Message<String>> stockInfoEventSupplier() {
-		return message -> log.info("Budget-Tracking-Service has received the event with payload: " + message.getPayload());
 	}
 
 	@Bean
